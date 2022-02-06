c134a3ec1833907357f46c0fd02225992ef4bca1
Gabi Badescu <gabi_badescu99@yahoo.com>
2022-02-06 02:10:34 +0200

    users statistics

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,8 +10,15 @@ export default function HomePage() {
 
     const [countUsers, setCountUsers] = useState(0);
     let i = 0;
-    let countLight = 0, countNone = 0, countModerate = 0, countHeavy = 0;
-    let countLose = 0, countMantain = 0, countGain = 0;
+    const [countLight,setCountLight]=useState(0);
+    const [countNone,setCountNone]=useState(0);
+    const [countModerate,setCountModerate]=useState(0);
+    const [countHeavy,setCountHeavy]=useState(0);
+    const [countLose,setCountLose]=useState(0);
+    const [countMaintain,setCountMaintain]=useState(0);
+    const [countGain,setCountGain]=useState(0);
+    let countLight1 = 0, countNone1 = 0, countModerate1 = 0, countHeavy1 = 0;
+    let countLose1 = 0, countMaintain1 = 0, countGain1 = 0;
 
     function printUsers() {
 
@@ -21,8 +28,38 @@ export default function HomePage() {
                 snapshot.forEach((snap) => {
                     const userObject = snap.val();
                     i++;
-                    setCountUsers(countUsers + i);
                     console.log(userObject["activity"]);
+                    setCountUsers(countUsers + i);
+                    if(userObject["activity"]==="none"){
+                        countNone1++;
+                        setCountNone(countNone+countNone1);
+                    }
+                    if(userObject["activity"]==="light"){
+                        countLight1++;
+                        setCountLight(countLight+countLight1);
+                    }
+                    if(userObject["activity"]==="moderate"){
+                        countModerate1++;
+                        setCountModerate(countModerate+countModerate1);
+                    }
+                    if(userObject["activity"]==="heavy"){
+                        countHeavy1++;
+
+                        console.log(countHeavy1);
+                        setCountHeavy(countHeavy+countHeavy1);
+                    }
+                    if(userObject["activity"]==="lose"){
+                        countLose1++;
+                        setCountLose(countLose+countLose1);
+                    }
+                    if(userObject["activity"]==="maintain"){
+                        countNone1++;
+                        setCountMaintain(countMaintain+countMaintain1);
+                    }
+                    if(userObject["activity"]==="gain"){
+                        countGain1++;
+                        setCountGain(countGain+countGain1);
+                    }
                 });
             });
     }
@@ -40,6 +77,16 @@ export default function HomePage() {
                 <h1>Welcome to Healthy Way</h1>
                 <p>Healthy way is an online tool which calculates your daily calories based on your goals</p>
                 <p>Currently there are {countUsers} users registered in our app</p>
+                <p>Our users have set the following activity:</p>
+
+                    <ul>{countNone} doesn't have activity</ul>
+                    <ul>{countLight} have light activity</ul>
+                    <ul>{countModerate} have moderate activity</ul>
+                    <ul>{countHeavy} have hard activity</ul>
+              <p>And the following objectives:</p>
+                    <ul>{countLight} have loss weight objective</ul>
+                    <ul>{countMaintain} have to keep their current weight </ul>
+                    <ul>{countHeavy} want to gain weight</ul>
             </div>
             <Footer/>
         </div>
